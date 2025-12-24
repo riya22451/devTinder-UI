41842e380f6ea7c38a78c6bfc910f3dd410d0566
riya22451 <riyaaggarwal226@gmail.com>
2025-12-24 12:30:29 +0530

    backend

--- devTinder-UI/src/utils/constants.js
+++ devTinder-UI/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://dev-tinder-backend-taupe.vercel.app/';
+export const API_BASE_URL = 'https://devtinder-backend-zyql.onrender.com/';
--- devTinder-UI/src/utils/socket.js
+++ devTinder-UI/src/utils/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client"
 export const createSocketConnection=()=>{
-    return io("https://dev-tinder-backend-taupe.vercel.app")
+    return io("https://devtinder-backend-zyql.onrender.com")
 }
