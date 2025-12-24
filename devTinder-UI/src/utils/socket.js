973c24d1b7fd0c22ec82e6d5802750a8ec508638
riya22451 <riyaaggarwal226@gmail.com>
2025-12-24 19:20:32 +0530

    bug fix

--- devTinder-UI/src/utils/socket.js
+++ devTinder-UI/src/utils/socket.js
@@ -1,3 +1,9 @@
-import  io from "socket.io-client";
+import { io } from "socket.io-client";
 
-export const createSocketConnection = io("https://dev-tinder-backend-taupe.vercel.app");
+export const socket = io(
+  "https://dev-tinder-backend-taupe.vercel.app",
+  {
+    withCredentials: true,
+    transports: ["polling"] // needed because backend is on Vercel
+  }
+);
