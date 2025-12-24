85cc7319f8c4611343ee21adc1d0d87fcb233102
riya22451 <riyaaggarwal226@gmail.com>
2025-12-24 19:21:41 +0530

    production erro

--- devTinder-UI/src/utils/socket.js
+++ devTinder-UI/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
+export const createSocketConnection = io(
   "https://dev-tinder-backend-taupe.vercel.app",
   {
     withCredentials: true,
